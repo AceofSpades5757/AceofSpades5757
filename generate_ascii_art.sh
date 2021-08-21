f49f1b03bfc689a72bab4e7c41a807bd48239b35
AceofSpades5757 <10341888+AceofSpades5757@users.noreply.github.com>
2021-08-21 00:49:48 -0500

    Update banner width

--- generate_ascii_art.sh
+++ generate_ascii_art.sh
@@ -1,4 +1,5 @@
 # sudo apt install figlet boxes --yes
 #figlet README | boxes -d parchment
-# -t - Use full width of terminal (no word wrapping)
-figlet AceofSpades5757 -t | boxes -d parchment
+#figlet AceofSpades5757 -t | boxes -d parchment
+# Max length should be ~124
+figlet AceofSpades5757 -w 124 | boxes -s 124 -a hc -d parchment
