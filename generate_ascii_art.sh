c0f91a8ce0ea3080be3c3a45af7092bccb2e30e5
AceofSpades5757 <10341888+AceofSpades5757@users.noreply.github.com>
2021-08-20 22:51:58 -0500

    Update ascii art to username

--- generate_ascii_art.sh
+++ generate_ascii_art.sh
@@ -1,2 +1,4 @@
 # sudo apt install figlet boxes --yes
-figlet README | boxes -d parchment
+#figlet README | boxes -d parchment
+# -t - Use full width of terminal (no word wrapping)
+figlet AceofSpades5757 -t | boxes -d parchment
